9fa95f5d388f3f6fc5c3f492e44d4d22eb977175
Jason Ray <jasonray@jayray.net>
2019-03-27 20:31:18 -0400

    formatting cleanup

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var httpPort = process.env.PORT || pargv.p || 80;
 var defaultDelay = pargv.d || 1000;
 var defaultContent = pargv.c || '';
 var defaultResponseHttpStatus = pargv.s || 202;
- 
+
 var express = require('express');
 var app = express();
 
@@ -20,17 +20,17 @@ console.log('default delay set to ' + defaultDelay);
 console.log('default content to ' + defaultContent);
 
 // this would represent an expensive resource with latency
-app.get('/', function(req, res, next) {
-	var delay = req.query.delay || defaultDelay;
-	var responseContent = req.query.content || defaultContent;
-	var responseHttpStatus = req.query.status || defaultResponseHttpStatus;
+app.get('/', function (req, res, next) {
+    var delay = req.query.delay || defaultDelay;
+    var responseContent = req.query.content || defaultContent;
+    var responseHttpStatus = req.query.status || defaultResponseHttpStatus;
 
-	setTimeout(function() {
-		// logger.info('end fetch data');
-		res.status(responseHttpStatus).end(responseContent);
-	}, delay);
+    setTimeout(function () {
+        // logger.info('end fetch data');
+        res.status(responseHttpStatus).end(responseContent);
+    }, delay);
 });
 
-app.listen(httpPort, function() {
-	console.log("now listening on %s", httpPort);
+app.listen(httpPort, function () {
+    console.log("now listening on %s", httpPort);
 });
